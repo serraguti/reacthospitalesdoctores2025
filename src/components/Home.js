aedd963503a996a9d4aaae017d901e3ff6e0da1b
serraguti <pacoserranox@gmail.com>
2025-10-21 10:13:18 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{color:"blue"}}>
+            Home hospitales
+        </h1>
+      </div>
+    )
+  }
+}
